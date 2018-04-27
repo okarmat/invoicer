66ac300f214456bbec57827e7db468cd010769b5
Tom <tomek.okarma@gmail.com>
2018-04-27 21:22:38 +0200

    creating upload controller

---
+++ Invoicer/Invoicer/Controllers/UploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Invoicer.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if(file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "Plik załadowany poprawnie.";
+                return View();
+            }
+            catch(Exception exc)
+            {
+                ViewBag.Message = "Wystąpił błąd podczas ładowania pliku.";
+                return View();
+            }
+        }
+    }
+}
