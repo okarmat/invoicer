450bcbfe7470537be266e2efebe159e4c83db63b
Tom <tomek.okarma@gmail.com>
2018-04-29 13:08:47 +0200

    creating Fuelnvoice controller and view

---
+++ Invoicer/Invoicer/Controllers/FuelInvoiceController.cs
@@ -0,0 +1,65 @@
+﻿using Invoicer.Models;
+using Invoicer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+
+namespace Invoicer.Controllers
+{
+    public class FuelInvoiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FuelInvoiceController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new FuelInvoiceFormViewModel
+            {
+                CurrencyTypes = _unitOfWork.CurrencyTypes.GetCurrencyTypes(),
+                FuelTypes = _unitOfWork.FuelTypes.GetFuelTypes(),
+                GasStations = _unitOfWork.GasStations.GetGasStations()
+            };
+
+            return View("Create", viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(FuelInvoiceFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.FuelTypes = _unitOfWork.FuelTypes.GetFuelTypes();
+                viewModel.CurrencyTypes = _unitOfWork.CurrencyTypes.GetCurrencyTypes();
+                viewModel.GasStations = _unitOfWork.GasStations.GetGasStations();
+                return View("Create", viewModel);
+            }
+
+            var fuelInvoice = new FuelInvoice
+            {
+                UserId = User.Identity.GetUserId()
+            };
+
+            _unitOfWork.FuelInvoices.Add(fuelInvoice);
+            _unitOfWork.Complete();
+
+            return RedirectToAction("Index", "Manage"); ;
+        }
+    }
+}
---
+++ Invoicer/Invoicer/ViewModels/FuelInvoiceFormViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Invoicer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Invoicer.ViewModels
+{
+    public class FuelInvoiceFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int FuelType { get; set; }
+
+        [Required]
+        public string RefuelingDate { get; set; }
+
+        [Required]
+        [RegularExpression("(\\d{1,8}\\,\\d{1,6})", ErrorMessage = "Wprowadź cenę")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int CurrencyType { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        [Required]
+        public decimal Quantity { get; set; }
+
+        public int GasStation { get; set; }
+
+        [Required]
+        public int MeterStatus { get; set; }
+
+        public IEnumerable<FuelType> FuelTypes { get; set; }
+
+        public IEnumerable<CurrencyType> CurrencyTypes { get; set; }
+
+        public IEnumerable<GasStation> GasStations { get; set; }
+    }
+}
