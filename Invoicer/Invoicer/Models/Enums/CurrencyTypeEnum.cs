6f284274b99477390c127e2fa6cb774171825f36
Tom <tomek.okarma@gmail.com>
2018-05-02 13:36:00 +0200

    creating CurrencyTypeEnum

---
+++ Invoicer/Invoicer/Models/Enums/CurrencyTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Invoicer.Models.Enums
+{
+    public enum CurrencyTypeEnum
+    {
+        Unknown = 0,
+        PLN = 1,
+        USD = 2,
+        EUR = 3
+    }
+}
