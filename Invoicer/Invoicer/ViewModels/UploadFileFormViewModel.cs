c3a1972a541802fdfaa72613ebe4560753f199fc
Tom <tomek.okarma@gmail.com>
2018-05-02 16:14:48 +0200

    improving validation for create and uploadFile views

--- Invoicer/Invoicer/ViewModels/UploadFileFormViewModel.cs
+++ Invoicer/Invoicer/ViewModels/UploadFileFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Invoicer.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Invoicer.ViewModels
 {
     public class UploadFileFormViewModel
     {
+        [Required]
         public int InvoiceType { get; set; }
 
         public IEnumerable<InvoiceType> InvoiceTypes { get; set; }
